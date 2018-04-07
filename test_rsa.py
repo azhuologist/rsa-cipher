643350b802da41e5b2d0eefe4bc060bc8a4ae68a
Andy <az389@cornell.edu>
2018-04-07 15:10:41 -0400

    Improves tests

--- test_rsa.py
+++ test_rsa.py
@@ -6,16 +6,19 @@ import os
 import filecmp
 import RSACipher
 
-ciph = RSACipher.RSACipher()
-ciph.encrypt('resources/test/Wallpaper.png', 'encryptedimage.png')
-ciph.decrypt('encryptedimage.png', 'result.png')
-
-if filecmp.cmp('resources/test/Wallpaper.png', 'result.png'):
-    print('Success!')
-
-os.remove('encryptedimage.png')
-os.remove('result.png')
-
 class BasicTests(unittest.TestCase):
-    pass
-    
+
+    def test_image(self):
+        """Runs encryption and then decryption on a small PNG file."""
+        ciph = RSACipher.RSACipher()
+        ciph.encrypt('resources/test/Wallpaper.png', 'encryptedimage.png')
+        ciph.decrypt('encryptedimage.png', 'result.png')
+
+        self.assertTrue(filecmp.cmp('resources/test/Wallpaper.png', 'result.png'))
+
+    def tearDown(self):
+        os.remove('encryptedimage.png')
+        os.remove('result.png')
+
+if __name__ == '__main__':
+    unittest.main()
